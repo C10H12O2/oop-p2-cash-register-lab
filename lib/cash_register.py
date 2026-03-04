b6f476dc6bbb91dc467282978d107e8002f3751e
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-03-04 17:02:23 +0300

    Implemented CashRegister init and discount validation logic

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.items = []
+    self.total = 0
+    self.previous_transactions = []
+    
+  @property
+  def discount(self):
+    return self._discount
+  
+  @discount.setter
+  def discount(self, value):
+    if isinstance(value, int) and 0 <= value <= 100:
+      self._discount = value
+    else:
+      print("Not a valid discount")
+
