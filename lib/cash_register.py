eac81dc8656b1335f602e24e47a1ab7984a93db7
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-03-04 23:37:27 +0300

    Made a few more changes for grading

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,45 +1,44 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  def __init__(self, discount=0):
-    self.discount = discount
-    self.items = []
-    self.total = 0
-    self.previous_transactions = []
-    
-  @property
-  def discount(self):
-    return self._discount
-  
-  @discount.setter
-  def discount(self, value):
-    if isinstance(value, int) and 0 <= value <= 100:
-      self._discount = value
-    else:
-      print("Not a valid discount")
-      
-  def add_item(self, item, price, quantity=1):
-    self.total += (price * quantity)
-    
-    for _ in range(quantity):
-      self.items.append(item)
-      
-    transaction = {
-      "item": item,
-      "price": price,
-      "quantity": quantity
-    }
-    self.previous_transactions.append(transaction)
-    
-  def apply_discount(self):
-    if self.discount > 0:
-      self.total = self.total * (1 - (self.discount / 100))
-      return self.total
-    else:
-      print("There is no discount to apply.")
-      return self.total
-    
-def void_last_transaction(self):
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
+
+    def add_item(self, item, price, quantity=1):
+        self.total += (price * quantity)
+        
+        for _ in range(quantity):
+            self.items.append(item)
+            
+        transaction = {
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        }
+        self.previous_transactions.append(transaction)
+
+    def apply_discount(self):
+        if self.discount > 0:
+            self.total = self.total * (1 - (self.discount / 100))
+            print(f"After the discount, the total comes to ${int(self.total)}.")
+            return self.total
+        else:
+            print("There is no discount to apply.")
+            return self.total
+
+    def void_last_transaction(self):
         if self.previous_transactions:
             last = self.previous_transactions.pop()
             
